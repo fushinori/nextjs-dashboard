f1297e91c3d1f6be1fddeaa6f80040cd3d168f51
Rishi <git@fushinori.me>
2025-03-16 15:59:05 +0530

    Add server action for deleting invoice

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -56,3 +56,10 @@ export async function updateInvoice(id: string, formData: FormData) {
   revalidatePath("/dashboard/invoices");
   redirect("/dashboard/invoices");
 }
+
+export async function deleteInvoice(id: string) {
+  await sql`DELETE FROM invoices WHERE id = ${id}`;
+  revalidatePath("/dashboard/invoices");
+
+  // No need to redirect since we're already in /dashboard/invoices
+}
